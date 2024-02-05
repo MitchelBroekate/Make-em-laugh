d775abd8c90c95e76ac1edea30c53bd6de4bef40
MitchelBroekate <mitchelb11@outlook.com>
2024-02-05 16:47:25 +0100

    click mechanic

--- Skeleton Fun/Assets/M/Scripts/ClickUp.cs
+++ Skeleton Fun/Assets/M/Scripts/ClickUp.cs
@@ -1,18 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
 
 public class ClickUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    RaycastHit hit;
+    public Interactions action;
+    bool clickCheck;
+    int randomI;
+    float randomF;
+
+   
+    private void Awake()
     {
-        
+        action = new Interactions();
+
+        action.Click.LeftClick.performed += x => Click();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (clickCheck)
+        {
+            StartCoroutine(ClickCooldown(randomF));
+        }
+
+
+    }
+    public void Click()
+    {
+        if (!clickCheck) 
+        {
+
+            randomI = Random.Range(1, 3);
+            randomF = Random.Range(1f, 3f);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 10000))
+            {
+                if (hit.transform.tag == "Player")
+                {
+
+                    if (randomI == 1)
+                    {
+                        Debug.Log("right");
+                        hit.rigidbody.velocity = new Vector3(15, 15, 0);
+                    }
+                    else
+                    {
+                        Debug.Log("left");
+                        hit.rigidbody.velocity = new Vector3(-15, 15, 0);
+                    }
+
+                    clickCheck = true;
+                }
+            }
+        }
+    }
+
+    IEnumerator ClickCooldown(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        clickCheck = false;
+        StopAllCoroutines();
+    }
+
+    private void OnEnable()
+    {
+        action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        action.Disable();
     }
 }
