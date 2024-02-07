b55784b2a991b78bdb8c5156d9e731944cd1ee46
MacKenzie1976 <97097945@st.deltion.nl>
2024-02-07 11:18:55 +0100

    ADS 0-5
    ADS YIPEEEE!!!!!!!!!!!!!!

---
+++ Skeleton Fun/Assets/UI/UI Scripts/GifAudio.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GifAudio : MonoBehaviour
+{
+    public AudioSource gifAudio; 
+
+    void Start()
+    {
+        gifAudio.Play(); 
+       
+    }
+
+    
+    
+}
